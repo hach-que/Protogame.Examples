fb9a2d5444615d2753c79f935d91cdfad17ab414
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-07-14 03:32:26 +1000

    Initialized example repository for Protogame 2

---
+++ PlatformerDemo/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PlatformerDemo
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // TODO: Implement game.
+        }
+    }
+}
